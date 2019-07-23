1d6f92eca242495d0cbbd7b8800868d2e7598e6f
Jakub Juza <jakub.juza@artin.cz>
2019-07-23 08:20:31 +0200

    remove default secret, some renaming

--- scripts/cfg/gwrs.sh
+++ scripts/cfg/gwrs.sh
@@ -41,46 +41,46 @@ PASS_SUFFIX='pass'
 BACKUP_SUFFIX='~'
 
 # root CA selfsigned (in production use company's root certificate)
-ROOT_CA='OpenXPKI_CA-One_Root_CA'
+ROOT_CA='RoboAuto_CA-One_Root_CA'
 ROOT_CA_REQUEST="${SSL_REALM}/${ROOT_CA}.${REQUEST_SUFFIX}"
 ROOT_CA_KEY="${SSL_REALM}/${ROOT_CA}.${KEY_SUFFIX}"
 ROOT_CA_KEY_PASSWORD="${SSL_REALM}/${ROOT_CA}.${PASS_SUFFIX}"
 ROOT_CA_CERTIFICATE="${SSL_REALM}/${ROOT_CA}.${CERTIFICATE_SUFFIX}"
-ROOT_CA_SUBJECT='/CN=OpenXPKI CA-One Root CA 1'
-ROOT_CA_SERVER_FQDN='rootca.openxpki.net'
+ROOT_CA_SUBJECT='/CN=RoboAuto CA-One Root CA 1'
+ROOT_CA_SERVER_FQDN='rootca.roboauto.cz'
 
 # issuing CA signed by root CA above
-ISSUING_CA='OpenXPKI_CA-One_Issuing_CA'
+ISSUING_CA='RoboAuto_CA-One_Issuing_CA'
 ISSUING_CA_REQUEST="${SSL_REALM}/${ISSUING_CA}.${REQUEST_SUFFIX}"
 ISSUING_CA_KEY="${SSL_REALM}/${ISSUING_CA}.${KEY_SUFFIX}"
 ISSUING_CA_KEY_PASSWORD="${SSL_REALM}/${ISSUING_CA}.${PASS_SUFFIX}"
 ISSUING_CA_CERTIFICATE="${SSL_REALM}/${ISSUING_CA}.${CERTIFICATE_SUFFIX}"
-ISSUING_CA_SUBJECT='/DC=net/DC=openxpki/DC=ca-one/CN=OpenXPKI Issuing CA 1'
+ISSUING_CA_SUBJECT='/DC=net/DC=RoboAuto/DC=ca-gwrs/CN=RoboAuto Issuing CA 1'
 
 # SCEP registration authority certificate signed by root CA above
-SCEP='OpenXPKI_CA-One_SCEP_RA'
+SCEP='RoboAuto_CA-One_SCEP_RA'
 SCEP_REQUEST="${SSL_REALM}/${SCEP}.${REQUEST_SUFFIX}"
 SCEP_KEY="${SSL_REALM}/${SCEP}.${KEY_SUFFIX}"
 SCEP_KEY_PASSWORD="${SSL_REALM}/${SCEP}.${PASS_SUFFIX}"
 SCEP_CERTIFICATE="${SSL_REALM}/${SCEP}.${CERTIFICATE_SUFFIX}"
-SCEP_SUBJECT='/DC=net/DC=openxpki/DC=ca-one/CN=OpenXPKI CA-One SCEP RA 1'
+SCEP_SUBJECT='/DC=net/DC=RoboAuto/DC=ca-gwrs/CN=RoboAuto CA-One SCEP RA 1'
 
 # Apache WEB certificate signed by root CA above
-WEB='OpenXPKI_CA-One_Web_CA'
+WEB='RoboAuto_CA-One_Web_CA'
 WEB_REQUEST="${SSL_REALM}/${WEB}.${REQUEST_SUFFIX}"
 WEB_KEY="${SSL_REALM}/${WEB}.${KEY_SUFFIX}"
 WEB_KEY_PASSWORD="${SSL_REALM}/${WEB}.${PASS_SUFFIX}"
 WEB_CERTIFICATE="${SSL_REALM}/${WEB}.${CERTIFICATE_SUFFIX}"
-WEB_SUBJECT='/DC=net/DC=openxpki/DC=ca-one/CN=issuing.ca-one.openxpki.net'
-WEB_SERVER_FQDN='issuing.ca-one.openxpki.net'
+WEB_SUBJECT='/DC=net/DC=RoboAuto/DC=ca-gwrs/CN=issuing.ca-gwrs.roboauto.cz'
+WEB_SERVER_FQDN='issuing.ca-gwrs.roboauto.cz'
 
 # data vault certificate selfsigned
-DATAVAULT='OpenXPKI_CA-One_DataVault'
+DATAVAULT='RoboAuto_CA-One_DataVault'
 DATAVAULT_REQUEST="${SSL_REALM}/${DATAVAULT}.${REQUEST_SUFFIX}"
 DATAVAULT_KEY="${SSL_REALM}/${DATAVAULT}.${KEY_SUFFIX}"
 DATAVAULT_KEY_PASSWORD="${SSL_REALM}/${DATAVAULT}.${PASS_SUFFIX}"
 DATAVAULT_CERTIFICATE="${SSL_REALM}/${DATAVAULT}.${CERTIFICATE_SUFFIX}"
-DATAVAULT_SUBJECT='/DC=net/DC=openxpki/DC=ca-one/DC=OpenXPKI Internal/CN=OpenXPKI CA-One DataVault'
+DATAVAULT_SUBJECT='/DC=net/DC=RoboAuto/DC=ca-gwrs/DC=RoboAuto Internal/CN=RoboAuto CA-One DataVault'
 
 #
 # openssl.conf
@@ -95,7 +95,7 @@ DDAYS="$RDAYS" # 10 years datavault (same a root)
 
 # used by v3 extension for issuing ca certificate
 ROOT_CA_HTTP_URI="URI:http://${ROOT_CA_SERVER_FQDN}/CertEnroll"
-ROOT_CA_CERTIFICATE_STRING="OpenXPKI_CA-One_Root_CA"
+ROOT_CA_CERTIFICATE_STRING="RoboAuto_CA-One_Root_CA"
 ROOT_CA_CERTIFICATE_URI="${ROOT_CA_HTTP_URI}/${ROOT_CA_CERTIFICATE_STRING}.${CERTIFICATE_SUFFIX}"
 ROOT_CA_REVOCATION_URI="${ROOT_CA_HTTP_URI}/${ROOT_CA_CERTIFICATE_STRING}.${REVOCATION_SUFFIX}"
 
@@ -347,14 +347,14 @@ openxpkiadm certificate import --file "${SCEP_CERTIFICATE}" --realm "${REALM}" -
 openxpkiadm certificate import --file "${DATAVAULT_CERTIFICATE}" --realm "${REALM}" --token datasafe
 
 # Create symlinks for the aliases used by the default config
-ln -s "${ISSUING_CA_KEY}" "${SSL_REALM}/ca-one-signer-1.pem"
-ln -s "${SCEP_KEY}" "${SSL_REALM}/ca-one-scep-1.pem"
-ln -s "${DATAVAULT_KEY}" "${SSL_REALM}/ca-one-vault-1.pem"
+ln -s "${ISSUING_CA_KEY}" "${SSL_REALM}/ca-gwrs-signer-1.pem"
+ln -s "${SCEP_KEY}" "${SSL_REALM}/ca-gwrs-scep-1.pem"
+ln -s "${DATAVAULT_KEY}" "${SSL_REALM}/ca-gwrs-vault-1.pem"
 
 echo "Place web certificate, private key, ... in web server configuration to enable ssl on openxpki web pages!"
 echo ""
-echo "OpenXPKI configuration should be done now, 'openxpkictl start' to fire up server'"
+echo "RoboAuto configuration should be done now, 'openxpkictl start' to fire up server'"
 echo ""
-echo "Thanks for using OpenXPKI - Have a nice day ;)"
+echo "Thanks for using RoboAuto - Have a nice day ;)"
 echo ""
 
